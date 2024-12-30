3abcdb88e74c66ecf34a44292a8a9598f55b952a
TKAn08 <trankhanha53@gmail.com>
2024-12-30 23:44:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//Nhận tất cả giá trị nút bấm
+const buttons = document.querySelectorAll('.calc-button');
+//Nhận giá trị hiển thị
+const display = document.querySelector('.calc-display');
+
+let firstOperand = null;
+let operator = null;
+let currentOperand = "";
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const target = event.target;
+        const value = target.innerText;
+        if (target.id == 'clear') {
+            firstOperand = null;
+            operator = null;
+            currentOperand = ""
+            display.innerText = '0';
+        } else if (target.id == 'plus-minus') {
+            display.innerText = -1 * display.innerText;
+        } else if (target.id == 'percentage') {
+            display.innerText = parseFloat(display.innerText) / 100;
+        } else if (
+            target.id == 'divide' ||
+            target.id == 'multi' ||
+            target.id == 'minus' ||
+            target.id == 'plus'
+        ) {
+            operator = value;
+            firstOperand = parseFloat(display.innerText);
+            currentOperand = "";
+        } else if (target.id == 'equal') {
+            if (operator) {
+                const secondOperand = parseFloat(display.innerText);
+                if (operator == '+') {
+                    firstOperand = firstOperand + secondOperand;
+                } else if (operator == '-') {
+                    firstOperand = firstOperand - secondOperand;
+                } else if (operator == '×') {
+                    firstOperand = firstOperand * secondOperand;
+                } else if (operator == '÷') {
+                    firstOperand = firstOperand / secondOperand;
+                }
+                operator = null;
+                currentOperand = firstOperand.toString();
+                display.innerText = firstOperand;
+            }
+        } else {
+            if (value === '.' && currentOperand.includes(".")) {
+                return;
+            }
+            currentOperand += value;
+            display.innerText = currentOperand
+        }
+    })
+});
